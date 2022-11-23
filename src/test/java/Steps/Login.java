549837e01d4f371e7db71e58d85656bd9fbd0c52
Maan9 <wsmaan896@gmail.com>
2022-11-23 20:24:42 +0000

    Repo123/abc change the user message

--- src/test/java/Steps/Login.java
+++ src/test/java/Steps/Login.java
@@ -5,7 +5,8 @@ import io.cucumber.java.en.When;
 public class Login {
     @Given("^sample 1$")
     public void sample_1() throws Throwable {
-        System.out.println("sample 1");}
+        System.out.println("sample 1");
+        System.out.println("New comment");}
     @When("^sample 2$")
     public void sample_2() throws Throwable {
         System.out.println("sample 2");}
