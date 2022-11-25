25d3af19d3083137489f015e79294c679b0fc498
WSMaan <118893766+WSMaan@users.noreply.github.com>
2022-11-25 16:39:14 +0000

    upDate

--- src/test/java/Steps/Login.java
+++ src/test/java/Steps/Login.java
@@ -6,8 +6,7 @@ public class Login {
     @Given("^sample 1$")
     public void sample_1() {
         System.out.println("sample 1");
-        System.out.println("new comment");
-        System.out.println("Change in Maaaster");
+       
         System.out.println("New comment");}
     @When("^sample 2$")
     public void sample_2()  {
